053ed6f640e2661be4046a13a3be73aaede64855
evan999 <emeshberge@gmail.com>
2020-01-21 20:15:34 -0500

    Remove arrow in module.exports in User Registration route.

--- config/db.js
+++ config/db.js
@@ -3,22 +3,18 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-	try 
-	{
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		});
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
 
-
-		console.log('MongoDB connected...');
-	}
-	catch (err)
-	{
-	console.error(err.message);
-	// Exit process with failure
-	process.exit(1);
-	}
-}
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
