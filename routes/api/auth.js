9898814d537df834a790be4051c52dbbc53904bd
evan999 <emeshberge@gmail.com>
2020-01-23 21:20:05 -0500

    Add user auth routes. Auth route needs to be fixed.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const jwt = 'jsonwebtoken';
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // @route GET api/auth
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, (req, res) => res.send('Auth route'));
 
 module.exports = router;
